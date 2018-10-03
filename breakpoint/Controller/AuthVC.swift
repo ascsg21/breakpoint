e3ccbf1af37d2065ef704fbb54b374b05f55a48b
ascsg21 <lovelyroom77@gmail.com>
2018-10-03 15:02:06 +0900

    10월 3일 commit

--- breakpoint/Controller/AuthVC.swift
+++ breakpoint/Controller/AuthVC.swift
@@ -17,14 +17,15 @@ class AuthVC: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func signInWithEmailBtnWasPressed(_ sender: Any) {
+        let loginVC = storyboard?.instantiateViewController(withIdentifier: "LoginVC")
+        present(loginVC!, animated: true, completion: nil)
     }
-    */
-
+    
+    @IBAction func facebookSignInBtnWasPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func googleSignInBtnWasPressed(_ sender: Any) {
+    }
 }
---
+++ breakpoint/Controller/MeVC.swift
@@ -0,0 +1,26 @@
+//
+//  MeVC.swift
+//  breakpoint
+//
+//  Created by MinRiDaddy on 03/10/2018.
+//  Copyright © 2018 MinRiDaddy. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signOutBtnWasPressed(_ sender: Any) {
+    }
+}
