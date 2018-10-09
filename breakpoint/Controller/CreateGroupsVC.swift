750f6d34bd27270da5ae9e3cd4f8ccfc18c5feca
ascsg21 <lovelyroom77@gmail.com>
2018-10-09 16:29:13 +0900

    10월 9일 commit

---
+++ breakpoint/Controller/CreateGroupsVC.swift
@@ -0,0 +1,51 @@
+//
+//  CreateGroupsVC.swift
+//  breakpoint
+//
+//  Created by MinRiDaddy on 09/10/2018.
+//  Copyright © 2018 MinRiDaddy. All rights reserved.
+//
+
+import UIKit
+
+class CreateGroupsVC: UIViewController {
+
+    @IBOutlet weak var titleTextField: InsetTextField!
+    @IBOutlet weak var descriptionTextField: InsetTextField!
+    @IBOutlet weak var emailSearchTextField: InsetTextField!
+    
+    @IBOutlet weak var doneBtn: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var groupMemberLbl: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    @IBAction func doneBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func closeBtnWasPressed(_ sender: Any) {
+    }
+}
+
+extension CreateGroupsVC: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "userCell") as? UserCell else { return UITableViewCell() }
+        let profileImage = UIImage(named: "defaultProfileImage")
+        
+        cell.configureCell(profileImage: profileImage!, email: "aa@aa.com", isSelected: true)
+        return cell
+        
+    }
+}
