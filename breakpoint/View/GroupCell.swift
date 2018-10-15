e9374dae922597b580be88bfaff45b4726a101c2
ascsg21 <lovelyroom77@gmail.com>
2018-10-15 20:43:20 +0900

    10월 15일 commit

---
+++ breakpoint/View/GroupCell.swift
@@ -0,0 +1,23 @@
+//
+//  GroupCell.swift
+//  breakpoint
+//
+//  Created by MinRiDaddy on 15/10/2018.
+//  Copyright © 2018 MinRiDaddy. All rights reserved.
+//
+
+import UIKit
+
+class GroupCell: UITableViewCell {
+
+    @IBOutlet weak var groupTitleLbl: UILabel!
+    @IBOutlet weak var groupDescLbl: UILabel!
+    @IBOutlet weak var memberCountLbl: UILabel!
+    
+
+    func configureCell(title: String, description: String, memberCount: Int) {
+        self.groupTitleLbl.text = title
+        self.groupDescLbl.text = description
+        self.memberCountLbl.text = "\(memberCount) members."
+    }
+}
