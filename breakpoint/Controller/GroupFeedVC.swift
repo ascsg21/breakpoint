7042c66df801bbcd8290e65a8093c6725337e264
ascsg21 <lovelyroom77@gmail.com>
2018-10-25 19:53:26 +0900

    10월 25일 commit

---
+++ breakpoint/Controller/GroupFeedVC.swift
@@ -0,0 +1,47 @@
+//
+//  GroupFeedVC.swift
+//  breakpoint
+//
+//  Created by MinRiDaddy on 25/10/2018.
+//  Copyright © 2018 MinRiDaddy. All rights reserved.
+//
+
+import UIKit
+
+class GroupFeedVC: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var groupTitleLbl: UILabel!
+    @IBOutlet weak var membersLbl: UILabel!
+    @IBOutlet weak var sendBtnView: UIView!
+    @IBOutlet weak var messageTextField: InsetTextField!
+    @IBOutlet weak var sendBtn: UIButton!
+    
+    var group: Group?
+    
+    func initGroupData(forGroup group: Group) {
+        self.group = group
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sendBtn.bindToKeyboard()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        groupTitleLbl.text = group?.groupTitle
+        DataService.instance.getEmailsFor(group: group!) { (returnedEmails) in
+            self.membersLbl.text = returnedEmails.joined(separator: ", ")
+        }
+    }
+    
+    @IBAction func sendBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ breakpoint/View/GroupFeedCell.swift
@@ -0,0 +1,22 @@
+//
+//  GroupFeedCell.swift
+//  breakpoint
+//
+//  Created by MinRiDaddy on 25/10/2018.
+//  Copyright © 2018 MinRiDaddy. All rights reserved.
+//
+
+import UIKit
+
+class GroupFeedCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var contentLbl: UILabel!
+    
+    func confifureCell(profileImage: UIImage, email: String, content: String) {
+        self.profileImage.image = profileImage
+        self.emailLbl.text = email
+        self.contentLbl.text = content
+    }
+}
